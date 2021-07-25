9e51d43e265618dd30c740961f4ec6a8af625dde
ryan <ryanyogan@gmail.com>
2021-07-24 22:18:23 -0700

    updated the host;

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :crypto_watch, CryptoWatchWeb.Endpoint,
-  url: [host: "leafy-crafty-bellfrog.com", port: 80],
+  url: [host: "leafy-crafty-bellfrog.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
